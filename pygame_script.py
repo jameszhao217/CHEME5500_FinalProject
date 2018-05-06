a2fac85a078bf7851e9a14e11b4a5274ca9be9f9
jameszhao217 <mz467@cornell.edu>
2018-05-06 14:50:17 -0400

    Pygame Update 3.0 - Error Solved

--- pygame_script.py
+++ pygame_script.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pygame
+import sys
 
 # PyGame Tutorial Reference: 
 # https://pythonprogramming.net/pygame-buttons-part-1-button-rectangle/
@@ -17,14 +18,13 @@ def pygame_plot():
     black = (0,0,0)
     white = (255,255,255)
     red = (200,0,0)
-    green = (0,200,0)
-    
+    green = (0,200,0)    
     bright_red = (255,0,0)
     bright_green = (0,255,0)
     
     # Set display
     gameDisplay = pygame.display.set_mode((display_width,display_height))
-    clock = pygame.time.Clock()
+#    clock = pygame.time.Clock()
     
     # Name program
     pygame.display.set_caption('Beam Deflection')
@@ -41,15 +41,28 @@ def pygame_plot():
         textSurface = font.render(text, True, black)
         return textSurface, textSurface.get_rect()
         
-    # def message_display(text):
-    #    largeText = pygame.font.Font('freesansbold.ttf',115)
-    #    TextSurf, TextRect = text_objects(text, largeText)    
-    #    TextRect.center = ((display_width/2),(display_height/2))
-    #    gameDisplay.blit(TextSurf, TextRect)
-    #    pygame.display.update
-    #    time.sleep(2)
-    #    game_loop()
-    
+    # Define GUI button function
+    def button(msg,x,y,w,h,low_color,high_color,action = None):
+            
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if x+w/2 > mouse[0] > x-w/2 and y + h > mouse[1] > y:
+            pygame.draw.rect(gameDisplay, low_color, (x-w/2,y,w,h))
+            if click[0] == 1 and action != None:
+                if action == 'Play':
+                    game_loop()
+                elif action == 'Quit':
+                    pygame.quit()
+                    sys.exit()         
+        else:
+            pygame.draw.rect(gameDisplay, high_color, (x-w/2,y,w,h))
+        
+        smallText = pygame.font.Font('freesansbold.ttf',20)
+        textSurf, textRect = text_objects(msg, smallText)
+        textRect.center = ( x, y + h/2)
+        gameDisplay.blit(textSurf, textRect)
+        
     # Game Start Menu    
     def game_intro():
         
@@ -59,44 +72,26 @@ def pygame_plot():
     
             event = pygame.event.poll()
             if event.type == pygame.QUIT:            
+                intro = False
                 pygame.quit()
-                break
+                sys.exit()
 
-#            for event in pygame.event.get():
-#                
-#                if event.type == pygame.QUIT:
-#                    intro = False
-#                    pygame.quit()                
-#                    break
-        
             gameDisplay.fill(white)
-            largeText = pygame.font.Font('freesansbold.ttf',55)
+            largeText = pygame.font.Font('freesansbold.ttf',40)
             TextSurf, TextRect = text_objects('Beam Deflection Curve Fun', largeText)    
             TextRect.center = ((display_width/2),(display_height/3))
             gameDisplay.blit(TextSurf, TextRect)
 
-            mouse = pygame.mouse.get_pos()
-            print(mouse)
-
-            if (display_width/2)+100 > mouse[0] > (display_width/2)-100 and (3*display_height/5) + 50 > mouse[1] > (3*display_height/5):
-                pygame.draw.rect(gameDisplay, bright_green, ((display_width/2)-100,(3*display_height/5),200,50))
-            else:
-                pygame.draw.rect(gameDisplay, green, ((display_width/2)-100,(3*display_height/5),200,50))
+            button('YES PLEASE',(display_width/2),3*display_height/5,200,50,bright_green,green,'Play')            
+            button('Nevermind',(display_width/2),3*display_height/5+100,200,50,bright_red,red,'Quit')
             
-            if (display_width/2)+100 > mouse[0] > (display_width/2)-100 and (3*display_height/5) + 150 > mouse[1] > (3*display_height/5)+100:
-                pygame.draw.rect(gameDisplay, bright_red, ((display_width/2)-100,(3*display_height/5)+100,200,50))
-            else:
-                pygame.draw.rect(gameDisplay, red, ((display_width/2)-100,(3*display_height/5)+100,200,50))
-
-
             pygame.display.update()
-            clock.tick(15)        
-#            game_loop()    
 
+    # Display Deflection Curve to Window   
     def game_loop():
             
         x = (display_width * 0.25)
-        y = (display_height * 0.5)
+        y = (display_height * 0.3)
           
         game_exit = False
         
@@ -104,26 +99,16 @@ def pygame_plot():
     
             event2 = pygame.event.poll()
             if event2.type == pygame.QUIT:
-                pygame.quit()
                 game_exit = True
-                break
-            
-    #        for event in pygame.event.get():
-    #            if event.type == pygame.QUIT:
-    ##                game_exit = True
-    #                pygame.quit()
-    #                break
+                pygame.quit()
+                sys.exit()
             
             gameDisplay.fill(white)
             defl_location(x,y)
             pygame.display.update()
-            clock.tick(60)
-        
-        
     
     # Run game
     game_intro()
-    #game_loop()
-    #
-    #pygame.quit()
-    #break
+
+    # Terminate Pygame
+    pygame.quit()
